f66f06b7dfc9d1ebadc0425869c2a03a5565f2e8
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-11 01:34:14 -0500

    made code working for question 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var changeText= document.querySelector(".first");
 var changeText2= document.querySelector(".firs");
+var changeText3= document.querySelector(".fir");
 
 function For(){
     changeText.innerHTML="I'm right"
@@ -8,3 +9,9 @@ function For(){
 function Again(){
     changeText2.innerHTML="No I'm right"
 }
+
+changeText3.addEventListener("mouseover", event => {
+    alert("Hey, I told you not to hover over me!");
+  });
+
+
