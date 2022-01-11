3e1ca2cac780ace3c53593e334edb61e5a32a09d
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-11 01:13:25 -0500

    made code working for first page question 1

---
+++ script.js
@@ -0,0 +1,10 @@
+var changeText= document.querySelector(".first");
+var changeText2= document.querySelector(".firs");
+
+function For(){
+    changeText.innerHTML="I'm right"
+}
+
+function Again(){
+    changeText2.innerHTML="No I'm right"
+}
